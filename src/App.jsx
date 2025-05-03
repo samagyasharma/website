a6b11d9711374d5da1a133a98e1958cb803939e9
samagyasharma <samagyasharma@gmail.com>
2025-05-03 20:32:00 +0530

    Made Gallery the default tab on opening the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import YourBag from "./pages/YourBag";
@@ -25,6 +25,7 @@ const App = () => {
               <Route path="/painting/:paintingId" element={<PaintingDetails />} />
               <Route path="/painting/:paintingId/zoom" element={<ZoomedImageView />} />
               <Route path="/order-confirmation" element={<OrderConfirmation />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
         </div>
