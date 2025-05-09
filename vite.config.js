f4e4c14cbb7843710fa77d1a364217b3078f434e
samagyasharma <samagyasharma@gmail.com>
2025-05-09 21:15:17 +0530

    chore: update project name to paintsandpixels

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/website/',
+  base: '/paintsandpixels/',
   plugins: [react()],
   server: {
     port: 3000,
